906e3bb2ce571963409d105b8a2dd4c1cb7b9d43
Oliver Levy <oliver.levy.ol@gmail.com>
2020-06-03 17:56:58 -0400

    updating js

--- sprint-2/js/script.js
+++ sprint-2/js/script.js
@@ -1,1 +1,40 @@
 console.log("hello world")
+
+
+
+// let user1 = {
+//   name: "Oliver Levy",
+//   comment: "wow I love this band so much! Holy moly I will got to all of their shows forever and ever and ever wowowowowow.",
+//   dateAded: "12/05/2020"
+// };
+
+// let user2 = {
+//   name: "Simone Ades",
+//   comment: "OMG WOWOWOW! I love this band so much! Holy moly I will got to all of their shows forever and ever and ever wowowowowow.",
+//   dateAded: "12/05/2020"
+// };
+
+// let user3 = {
+//   name: "Bill Whatshisname",
+//   comment: "I've seen better. Wow I love this band so much! Holy moly I will got to all of their shows forever and ever and ever wowowowowow.",
+//   dateAded: "12/05/2020"
+// };
+
+// let users = [user1, user2, user3]
+// console.log(users)
+
+// // pulls name from object
+// let inputName = user3.name;
+// let inputComment = user3.comment;
+// let inputDate = user3.dateAded;
+
+// // pulls element from html
+// let nameOutput = document.querySelector(".previous-comment__name");
+// let dateOutput = document.querySelector(".previous-comment__date");
+// let commentOutput = document.querySelector(".previous-comment__text");
+
+// console.log(nameOutput);
+
+// nameOutput.innerHTML = `${inputName}`;
+// dateOutput.innerHTML = `${inputDate}`;
+// commentOutput.innerHTML = `${inputComment}`;
